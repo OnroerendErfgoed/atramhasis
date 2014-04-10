022fd733653db9baa373a10ee131515b992868bd
Bart Saelen <bart.saelen@geosolutions.be>
2014-04-10 10:08:33 +0200

    project skeleton

---
+++ README.txt
@@ -0,0 +1,4 @@
+atramhasis README
+
+
+
---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'skosprovider', 
+    'skosprovider_sqlalchemy',
+    'pyramid_skosprovider',
+    'pyramid_jinja2'
+    ]
+
+setup(name='atramhasis',
+      version='0.0',
+      description='atramhasis',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='atramhasis',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = atramhasis:main
+      [console_scripts]
+      initialize_atramhasis_db = atramhasis.scripts.initializedb:main
+      """,
+      )
