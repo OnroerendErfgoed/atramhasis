7cdc6eee3d9c8ff0b1ec24149630921490ff30ea
Bart Saelen <bart.saelen@geosolutions.be>
2014-04-10 10:41:31 +0200

    jinja2 template

--- atramhasis/views.py
+++ atramhasis/views.py
@@ -9,7 +9,7 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='templates/atramhasis.jinja2')
 def my_view(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
