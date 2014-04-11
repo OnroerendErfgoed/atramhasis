45ef779412fdce2e86f82da05da3afb038d51a66
Bart Saelen <bart.saelen@geosolutions.be>
2014-04-11 16:32:55 +0200

    update tests

--- atramhasis/tests.py
+++ atramhasis/tests.py
@@ -5,6 +5,8 @@ from pyramid import testing
 
 from .models import DBSession
 
+from .scaffolds import AtramhasisTemplate, AtramhasisDemoTemplate
+
 
 class TestMyViewSuccessCondition(unittest.TestCase):
     def setUp(self):
@@ -53,3 +55,12 @@ class TestMyViewFailureCondition(unittest.TestCase):
         request = testing.DummyRequest()
         info = my_view(request)
         self.assertEqual(info.status_int, 500)
+
+class TestScaffolding(unittest.TestCase):
+    def test_scaffolding(self):
+        atemp= AtramhasisTemplate('test')
+        atempdemo= AtramhasisDemoTemplate('demo')
+        self.assertEqual(atemp.summary, 'Create an Atramhasis implementation')
+        self.assertEqual(atempdemo.summary, 'Create an Atramhasis demo')
+        self.assertEqual(atemp.name, 'test')
+        self.assertEqual(atempdemo.name, 'demo')
