b7c2eace27d25a3c6b741eab02682348ec697acd
Bart Saelen <bart.saelen@geosolutions.be>
2014-04-17 13:44:53 +0200

    fix iport sequence problem

--- atramhasis/__init__.py
+++ atramhasis/__init__.py
@@ -10,12 +10,12 @@ from .models import (
 def includeme(config):
     """this function adds some configuration for the application"""
     config.include('pyramid_jinja2')
-    config.include('pyramid_skosprovider')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('concept', pattern='/conceptschemes/{scheme_id}/c/{c_id}', accept='text/html')
     config.add_route('search_result', pattern='/conceptschemes/{scheme_id}/c', accept='text/html')
     config.add_route('foundation', '/foundation')
+    config.include('pyramid_skosprovider')
     config.scan('pyramid_skosprovider')
 
     config.scan()
