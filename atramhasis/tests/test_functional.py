166d345155511e38c7dde8af3b34c9d0d58ea30f
Bart Saelen <bart.saelen@geosolutions.be>
2014-04-25 17:10:38 +0200

    update tests
    #18

--- atramhasis/tests/test_functional.py
+++ atramhasis/tests/test_functional.py
@@ -1,15 +1,34 @@
 import os
 import unittest
+from skosprovider_sqlalchemy.models import Base
 from webtest import TestApp
 
 from pyramid import testing
 from atramhasis import main
 from pyramid.paster import get_appsettings
 from sqlalchemy import engine_from_config
+from atramhasis import DBSession
 
 TEST_DIR = os.path.dirname(__file__)
 settings = get_appsettings('atramhasis/tests/conf_test.ini')
 engine = engine_from_config(settings, prefix='sqlalchemy.')
+DBSession.configure(bind=engine)
+Base.metadata.create_all(engine)
+
+json_string = {
+    "type": "concept",
+    "broader": [],
+    "narrower": [],
+    "related": [],
+    "labels": [
+        {
+            "type": "prefLabel",
+            "language": "en",
+            "label": "The Larch"
+        }
+    ],
+    "notes": []
+}
 
 
 class FunctionalTests(unittest.TestCase):
@@ -39,3 +58,32 @@ class HtmlFunctionalTests(FunctionalTests):
         res = self.testapp.get('/', headers=self._get_default_headers())
         self.assertEqual('200 OK', res.status)
         self.assertIn('text/html', res.headers['Content-Type'])
+
+
+class RestFunctionalTests(FunctionalTests):
+
+    def _get_default_headers(self):
+        return {'Accept': 'application/json'}
+
+    def test_add_concept(self):
+        res = self.testapp.post_json('/conceptschemes/TREES/c', headers=self._get_default_headers(), params=json_string)
+        self.assertEqual('201 Created', res.status)
+        self.assertIn('application/json', res.headers['Content-Type'])
+
+    # def test_add_concept_invalid_json(self):
+    #     res = self.testapp.post_json(
+    #         '/conceptschemes/TREES/c', headers=self._get_default_headers(), params=json_string, status=400)
+    #     self.assertEqual('400 Created', res.status)
+    #     self.assertIn('application/json', res.headers['Content-Type'])
+    #
+    # def test_add_concept_conceptscheme_not_found(self):
+    #     res = self.testapp.post_json(
+    #         '/conceptschemes/GARDEN/c', headers=self._get_default_headers(), params=json_string, status=404)
+    #     self.assertEqual('404 Created', res.status)
+    #     self.assertIn('application/json', res.headers['Content-Type'])
+
+    def test_edit_concept(self):
+        res = self.testapp.put_json(
+            '/conceptschemes/TREES/c/1', headers=self._get_default_headers(), params=json_string)
+        self.assertEqual('200 OK', res.status)
+        self.assertIn('application/json', res.headers['Content-Type'])
--- atramhasis/tests/test_utils.py
+++ atramhasis/tests/test_utils.py
@@ -1,1 +1,55 @@
+import unittest
+from skosprovider_sqlalchemy.models import Concept, Collection, Note, Label
+from atramhasis.utils import from_thing
 
+from skosprovider.skos import(
+    Concept as SkosConcept,
+    Collection as SkosCollection
+)
+
+
+class TestFromThing(unittest.TestCase):
+    def setUp(self):
+        self.concept = Concept()
+        self.concept.type = 'concept'
+        self.concept.id = 11
+        self.concept.concept_id = 101
+        self.concept.uri = 'urn:x-atramhasis-demo:TREES:101'
+        self.concept.conceptscheme_id = 1
+
+        notes = []
+        note = Note(note='test note', notetype_id='example', language_id='en')
+        note2 = Note(note='note def', notetype_id='definition', language_id='en')
+        notes.append(note)
+        notes.append(note2)
+        self.concept.notes = notes
+
+        labels = []
+        label = Label(label='een label', labeltype_id='prefLabel', language_id='nl')
+        label2 = Label(label='other label', labeltype_id='altLabel', language_id='en')
+        label3 = Label(label='and some other label', labeltype_id='altLabel', language_id='en')
+        labels.append(label)
+        labels.append(label2)
+        labels.append(label3)
+        self.concept.labels = labels
+
+        self.collection = Collection()
+        self.collection.type = 'collection'
+        self.collection.id = 12
+        self.collection.concept_id = 102
+        self.collection.uri = 'urn:x-atramhasis-demo:TREES:102'
+        self.collection.conceptscheme_id = 1
+
+    def test_thing_to_concept(self):
+        skosconcept = from_thing(self.concept)
+        self.assertTrue(isinstance(skosconcept, SkosConcept))
+        self.assertEqual(skosconcept.id, 101)
+        self.assertEqual(len(skosconcept.labels), 3)
+        self.assertEqual(len(skosconcept.notes), 2)
+        self.assertEqual(skosconcept.uri, 'urn:x-atramhasis-demo:TREES:101')
+
+    def test_thing_to_collection(self):
+        skoscollection = from_thing(self.collection)
+        self.assertTrue(isinstance(skoscollection, SkosCollection))
+        self.assertEqual(skoscollection.id, 102)
+        self.assertEqual(skoscollection.uri, 'urn:x-atramhasis-demo:TREES:102')
