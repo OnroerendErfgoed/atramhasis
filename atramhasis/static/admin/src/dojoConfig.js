970a2fcc6608b632f477170530c462b1a8d1fe2e
Koen Van Daele <koen_van_daele@telenet.be>
2014-07-25 17:55:22 +0200

    Build supporting files.

---
+++ atramhasis/static/admin/src/dojoConfig.js
@@ -0,0 +1,16 @@
+/*jshint unused:false*/
+var dojoConfig = {
+	async: false,
+	parseOnLoad: false,
+	tlmSiblingOfDojo: false,
+	isDebug: true,
+	packages: [
+		'dojo',
+		'dijit',
+		'dojox',
+		'put-selector',
+		'xstyle',
+		'dgrid',
+		'app'
+	]
+};
