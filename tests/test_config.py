065e9d20e52f114031efcd6d44362a24e3620237
Bart Saelen <bart.saelen@geosolutions.be>
2014-10-24 15:22:11 +0200

    use in memory db for testing

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 from pyramid.paster import get_appsettings
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, create_engine
 from atramhasis import main
 from skosprovider_sqlalchemy.models import Base
 
@@ -10,11 +10,11 @@ settings = get_appsettings(os.path.join(here, '../', 'tests/conf_test.ini'))
 
 
 class TestConfig(unittest.TestCase):
-
     def setUp(self):
-        engine = engine_from_config(settings, prefix='sqlalchemy.')
-        Base.metadata.drop_all(engine)
-        Base.metadata.create_all(engine)
+        settings['sqlalchemy.url'] = 'sqlite:///%s/dbtest.sqlite' % here
+        self.engine = engine_from_config(settings, prefix='sqlalchemy.')
+        Base.metadata.drop_all(self.engine)
+        Base.metadata.create_all(self.engine)
 
     def test_config(self):
         app = main({}, **settings)
--- tests/test_skos.py
+++ tests/test_skos.py
@@ -1,14 +1,30 @@
+import os
 import unittest
 
 from pyramid import testing
+from pyramid.paster import get_appsettings
+from sqlalchemy import engine_from_config
 from sqlalchemy.orm import sessionmaker
+from atramhasis.models import Base
+from skosprovider_sqlalchemy.models import Base as SkosBase
 from atramhasis.skos import includeme
+
+here = os.path.dirname(__file__)
+settings = get_appsettings(os.path.join(here, '../', 'tests/conf_test.ini'))
 
 
 class TestSkos(unittest.TestCase):
     def setUp(self):
+        self.engine = engine_from_config(settings, 'sqlalchemy.')
+        Base.metadata.bind = self.engine
+        SkosBase.metadata.bind = self.engine
+        session_maker = sessionmaker(
+            bind=self.engine,
+        )
+        SkosBase.metadata.drop_all(self.engine)
+        SkosBase.metadata.create_all(self.engine)
         self.config = testing.setUp()
-        self.config.registry.dbmaker = sessionmaker()
+        self.config.registry.dbmaker = session_maker
 
     def tearDown(self):
         testing.tearDown()
