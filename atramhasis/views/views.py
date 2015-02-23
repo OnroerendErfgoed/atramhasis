04bc18ec47f1421d9c003cd7e53bde4d3d41d38b
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2015-02-23 11:27:11 +0100

    Update conceptschemes list. Refs #181.

--- atramhasis/views/views.py
+++ atramhasis/views/views.py
@@ -85,7 +85,7 @@ class AtramhasisView(object):
         '''
         conceptschemes = [
             {'id': x.get_metadata()['id'],
-             'uri': self.conceptscheme_manager.get(x.get_metadata()['conceptscheme_id']).uri}
+             'conceptscheme': self.conceptscheme_manager.get(x.get_metadata()['conceptscheme_id'])}
             for x in self.skos_registry.get_providers() if not 'external' in x.get_metadata()['subject']
         ]
 
