354e42bf7ea5bc8563168a7e5720434dbe60ae0d
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2015-08-31 13:14:28 +0200

    restyle header to match latest design

--- atramhasis/static/admin/src/app/ui/AppUi.js
+++ atramhasis/static/admin/src/app/ui/AppUi.js
@@ -138,6 +138,11 @@ define([
     _editLanguages: function (evt) {
       evt.preventDefault();
       console.debug('AppUi::_editLanguages');
+    },
+
+    _editConceptScheme: function (evt) {
+      evt.preventDefault();
+      console.debug('AppUi::_editConceptScheme');
     }
   });
 });
