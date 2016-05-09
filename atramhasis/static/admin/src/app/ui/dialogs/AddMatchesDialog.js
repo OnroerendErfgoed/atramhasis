f9490d572aff3560ed64270cc62fb055d63d1b7c
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2016-05-09 10:42:26 +0200

    More information on match when adding them #279

--- atramhasis/static/admin/src/app/ui/dialogs/AddMatchesDialog.js
+++ atramhasis/static/admin/src/app/ui/dialogs/AddMatchesDialog.js
@@ -96,6 +96,16 @@ define([
         },
         type: {
           label: ''
+        },
+        link: {
+          label: '',
+          renderCell: function(object){
+            console.log(object);
+            if (object && object.uri) {
+              return domConstruct.create('a', { href: object.uri, target: '_blank', title: object.uri,
+                innerHTML: '<i class="fa fa-external-link"></i>' });
+            }
+          }
         }
       };
 
