b8e13e26f2b5e88fa6daa5abfd7297cb215683a7
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2016-05-11 11:22:57 +0200

    manage languages

--- atramhasis/static/admin/src/app/ui/dialogs/ManageLanguagesDialog.js
+++ atramhasis/static/admin/src/app/ui/dialogs/ManageLanguagesDialog.js
@@ -4,15 +4,36 @@ define([
   'dijit/_WidgetsInTemplateMixin',
   'dijit/Dialog',
   'dojo/topic',
+  'dojo/_base/lang',
+  'dojo/query',
+  'dojo/on',
+  'dojo/when',
+  'dojo/_base/array',
+  'dojo/dom-construct',
+  'dgrid/OnDemandGrid',
+  'dgrid/extensions/DijitRegistry',
+  'dgrid/extensions/ColumnResizer',
   'dojo/text!./templates/ManageLanguagesDialog.html',
-  '../../utils/DomUtils'
+  'dijit/ConfirmDialog',
+  '../../utils/DomUtils',
+  'dojo/NodeList-manipulate'
 ], function (
   declare,
   _TemplatedMixin,
   _WidgetsInTemplateMixin,
   Dialog,
   topic,
+  lang,
+  query,
+  on,
+  when,
+  array,
+  domConstruct,
+  OnDemandGrid,
+  DijitRegistry,
+  ColumnResizer,
   template,
+  ConfirmDialog,
   DomUtils
 ) {
   return declare([Dialog, _TemplatedMixin, _WidgetsInTemplateMixin], {
@@ -27,30 +48,118 @@ define([
 
     postCreate: function () {
       this.inherited(arguments);
+
+      this._langStore = this.languageController.getLanguageStore();
+      this._langGrid = this._createGrid({
+        collection: this._langStore
+      }, this.langGridNode);
     },
 
     startup: function () {
       this.inherited(arguments);
+      this._langGrid.startup();
+      this._langGrid.resize();
+    },
 
+    _createGrid: function(options, node) {
+      var columns = {
+        id: {
+          label: "ID",
+          field: "id"
+        },
+        name: {
+          label: "Name",
+          field: "name"
+        },
+        actions: {
+          label: '',
+          renderCell: lang.hitch(this, function (object) {
+            if (object.id === undefined) {
+              return null;
+            }
+            var div = domConstruct.create('div', {'class': 'dGridHyperlink'});
+            domConstruct.create('a', {
+              href: '#',
+              title: 'Remove language',
+              className: 'fa fa-trash',
+              innerHTML: '',
+              onclick: lang.hitch(this, function (evt) {
+                evt.preventDefault();
+                this._removeRow(object);
+              })
+            }, div);
+            return div;
+          })
+        }
+      };
+
+      var grid = new (declare([OnDemandGrid, DijitRegistry, ColumnResizer]))({
+        collection: options.collection,
+        columns: columns,
+        noDataMessage: 'No languages found',
+        loadingMessage: 'Fetching data..'
+      }, node);
+
+      grid.on('dgrid-error', function(event) {
+        console.log(event.error.message);
+      });
+
+      return grid;
     },
 
     hide: function () {
       this.inherited(arguments);
-      //this._reset();
     },
 
     show: function () {
       this.inherited(arguments);
-      //this._reset();
+      this._reset();
     },
 
-    _okClick: function (evt) {
+    _addLangClick: function (evt) {
       console.debug('LanguagesDialog::_okClick');
       evt.preventDefault();
       if (this._validate()) {
-        //this.emit('add.label', {
-        //
-        //});
+        var obj = {
+          id: this.codeInputNode.value,
+          name: this.nameInputNode.value
+        };
+        when(this._langStore.add(obj)).then(
+          lang.hitch(this, function (lang) {
+            var message = 'New language added: ' + lang.name;
+            topic.publish('dGrowl', message, {
+              'title': 'Languages',
+              'sticky': false,
+              'channel': 'info'
+            });
+            this._reset();
+          }),
+          function (error) {
+            var errorJson = JSON.parse(error.response.data);
+            var message = "",
+              prop = null;
+            array.forEach(errorJson.errors, function (errorObj) {
+              for (prop in errorObj) {
+                message += "-<em>";
+                message += prop;
+                message += "</em>: ";
+                message += errorObj[prop];
+                message += "<br>";
+              }
+            });
+            topic.publish('dGrowl', message, {
+              'title': errorJson.message,
+              'sticky': true,
+              'channel': 'error'
+            });
+          }
+        );
+      } else {
+        topic.publish('dGrowl', '-Please fill in a language code and name to add a new language.', {
+              'title': 'Invalid values',
+              'sticky': true,
+              'channel': 'error'
+            });
       }
     },
 
@@ -61,11 +170,38 @@ define([
     },
 
     _reset: function () {
-
+      this._langGrid.refresh();
+      this.codeInputNode.value = '';
+      this.nameInputNode.value = '';
     },
 
     _validate: function () {
-      return true;
+      if (this.codeInputNode.value.trim() !== '' && this.codeInputNode.value.trim() !== '') {
+        return true;
+      }
+      return false;
+    },
+
+    _removeRow: function(language) {
+      var content = '<p style="font-size: 15px;">Are you sure you want to remove <strong>'+ language.name +
+        '</strong> (ID: ' + language.id + ')?</p>';
+
+      var confirmationDialog = new ConfirmDialog({
+        title: 'Delete language',
+        content: content,
+        baseClass: 'confirm-dialog'
+      });
+      query('.dijitButton', confirmationDialog.domNode).addClass('button tiny');
+      confirmationDialog.closeText.innerHTML = '<i class="fa fa-times"></i>';
+
+      on(confirmationDialog, 'close', function() {
+        confirmationDialog.destroy();
+      });
+      on(confirmationDialog, 'execute', lang.hitch(this, function () {
+        this._langStore.remove(language.id);
+      }));
+
+      confirmationDialog.show();
     }
   });
 });
