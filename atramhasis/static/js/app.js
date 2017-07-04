8480da25cd0e5ae0c45f265acac80d871f7b8c54
taeymma <maarten.taeymans@geosolutions.be>
2017-07-04 11:30:17 +0200

    Foutmelding geven bij zoeken op label zonder schema te verduidelijken #45

--- atramhasis/static/js/app.js
+++ atramhasis/static/js/app.js
@@ -4,28 +4,29 @@ $(document).foundation();
 
 // Function that makes the mobile menu work.
 $('#mobile-menu-select').change(function() {
-	window.location = $(this).val();
+  window.location = $(this).val();
 });
 
 $(document).ready(function () {
     /*
-    *  Set form action for label search form
-    *  Set href for scheme links
-    *  On startup and value change
-    */
-    setSchemeUrls( $("#scheme").val());
+     *  Set form action for label search form
+     *  Set href for scheme links
+     *  On startup and value change
+     */
+  setSchemeUrls( $("#scheme").val());
 
-    $("#scheme").change(function() {
-        var url = $("#scheme").val();
-        setSchemeUrls(url);
-    });
+  $("#scheme").change(function() {
+    var url = $("#scheme").val();
+    setSchemeUrls(url);
+  });
 });
 
 function setSchemeUrls(baseurl){
-    $("#search-form").attr("action", baseurl);
+  $("#search-form").attr("action", baseurl);
 }
 
 function getUrlVar(key){
-    var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
-    return result && unescape(result[1]) || "";
+  var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
+  return result && unescape(result[1]) || "";
 }
+
