7845e9e30fc3b1c723a26a5db1eb3dc112c034ee
Bart Saelen <bart.saelen@geosolutions.be>
2017-07-04 15:53:49 +0200

    cache regions #371

--- atramhasis/cache/__init__.py
+++ atramhasis/cache/__init__.py
@@ -1,7 +1,29 @@
 from dogpile.cache import make_region
 
-tree_cache_dictionary = {}
 tree_region = make_region()
+tree_cache_dictionary = {}
+
+list_region = make_region()
+list_cache_dictionary = {}
+
+
+def includeme(config):
+
+    if not tree_region.is_configured:
+        # tree_region.configure_from_config(kw, 'cache.') # https://github.com/OnroerendErfgoed/oeauth/blob/master/oeauth/oauth.py#L126
+        tree_region.configure(
+            'dogpile.cache.memory',
+            arguments={
+                "cache_dict": tree_cache_dictionary
+            }
+        )
+    if not list_region.is_configured:
+        list_region.configure(
+            'dogpile.cache.memory',
+            arguments={
+                "cache_dict": list_cache_dictionary
+            }
+        )
 
 
 def invalidate_scheme_cache(conceptscheme_id):
@@ -14,22 +36,3 @@ def invalidate_cache():
     keys = tree_cache_dictionary.keys()
     keys_to_delete = [key for key in keys]
     tree_region.delete_multi(keys_to_delete)
-
-
-list_cache_dictionary = {}
-list_region = make_region()
-
-
-def includeme(config):
-    tree_region.configure(
-        'dogpile.cache.memory',
-        arguments={
-            "cache_dict": tree_cache_dictionary
-        }
-    )
-    list_region.configure(
-        'dogpile.cache.memory',
-        arguments={
-            "cache_dict": list_cache_dictionary
-        }
-    )
