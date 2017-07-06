70c5d36325c54d2dd25895c01e0ffbe7112ef586
Bart Saelen <bart.saelen@geosolutions.be>
2017-07-06 17:06:36 +0200

    cache problems - invalidate region #371

--- atramhasis/cache/__init__.py
+++ atramhasis/cache/__init__.py
@@ -1,38 +1,22 @@
+# -*- coding: utf-8 -*-
 from dogpile.cache import make_region
 
 tree_region = make_region()
-tree_cache_dictionary = {}
-
 list_region = make_region()
-list_cache_dictionary = {}
 
 
 def includeme(config):
-
+    settings = config.registry.settings
     if not tree_region.is_configured:
-        # tree_region.configure_from_config(kw, 'cache.') # https://github.com/OnroerendErfgoed/oeauth/blob/master/oeauth/oauth.py#L126
-        tree_region.configure(
-            'dogpile.cache.memory',
-            arguments={
-                "cache_dict": tree_cache_dictionary
-            }
-        )
+        tree_region.configure_from_config(settings, 'cache.tree.')
     if not list_region.is_configured:
-        list_region.configure(
-            'dogpile.cache.memory',
-            arguments={
-                "cache_dict": list_cache_dictionary
-            }
-        )
+        list_region.configure_from_config(settings, 'cache.list.')
 
 
 def invalidate_scheme_cache(conceptscheme_id):
-    keys = tree_cache_dictionary.keys()
-    keys_to_delete = [k for k in keys if k.split("|")[1].split()[0] == conceptscheme_id]
-    tree_region.delete_multi(keys_to_delete)
+    # TODO implement propper conceptscheme cache invalidation if this is ever possible
+    tree_region.invalidate()
 
 
 def invalidate_cache():
-    keys = tree_cache_dictionary.keys()
-    keys_to_delete = [key for key in keys]
-    tree_region.delete_multi(keys_to_delete)
+    tree_region.invalidate()
