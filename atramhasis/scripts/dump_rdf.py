7d822d8292ab56033fa1a49e67f2d46d04544605
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2017-07-16 17:10:22 +0200

    Fix typo

--- atramhasis/scripts/dump_rdf.py
+++ atramhasis/scripts/dump_rdf.py
@@ -82,7 +82,7 @@ def main():
 
     for p in skos_registry.get_providers():
         if any([not_shown in p.get_metadata()['subject'] for not_shown in ['external', 'hidden']]):
-            continue;
+            continue
         start_time = time.time()
         pid = p.get_metadata()['id']
         filename = os.path.join(dump_location, '%s-full' % pid)
