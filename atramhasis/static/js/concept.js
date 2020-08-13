85d5e1b907f27250cc0f57e1586046430cbdc9e6
Yannick Kuypers <Kuypers.y@gmail.com>
2020-08-13 10:59:58 +0200

    #592 deleted history api

--- atramhasis/static/js/concept.js
+++ atramhasis/static/js/concept.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
     var searchUrl = window.location.search.substr(1);
-    const params = searchUrl.split('&');
+    const params = searchUrl.split();
 
-    if(params[1] === 'view_tree') {
+    if (params.findIndex(element => element === 'view_tree') > -1) {
       openTreeTab();
-      window.history.pushState('', null, window.location.href.split('?')[0]);
     }
   });
   
   function openTreeTab() {
-    document.getElementById('tree-link').click()
+    document.getElementById('tree-link').click();
   }
