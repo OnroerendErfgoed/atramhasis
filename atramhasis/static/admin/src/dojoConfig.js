ad66a159370c6d2d9b5923b2239fae44c8016efb
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2020-09-16 19:59:54 +0200

    remove unused packages from dojoConfig.js

--- atramhasis/static/admin/src/dojoConfig.js
+++ atramhasis/static/admin/src/dojoConfig.js
@@ -7,9 +7,6 @@ var dojoConfig = {
 	packages: [
 		{ name: 'dojo', location: '../node_modules/dojo' },
 		{ name: 'dijit', location: '../node_modules/dijit' },
-		{ name: 'dojox', location: '../node_modules/dojox' },
-		{ name: 'put-selector', location: '../node_modules/put-selector' },
-		{ name: 'xstyle', location: '../node_modules/xstyle' },
 		{ name: 'dgrid', location: '../node_modules/dgrid' },
 		{ name: 'dstore', location: '../node_modules/dstore' },
 		{ name: 'dGrowl', location: '../node_modules/dGrowl' },
