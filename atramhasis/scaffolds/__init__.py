9c2c9cd98aa6e6e8af3a7bbcd9b8315211f1b4af
Wim De Clercq <declewi@gmail.com>
2020-09-21 10:37:57 +0200

    remove unnecessary variables from scaffold.

--- atramhasis/scaffolds/__init__.py
+++ atramhasis/scaffolds/__init__.py
@@ -1,11 +1,7 @@
-import datetime
-import distutils.dir_util
-import os
 from textwrap import dedent
-
-from alembic.config import Config
-from pyramid.scaffolds import PyramidTemplate
-from pyramid.scaffolds import Template
+from pyramid.scaffolds import PyramidTemplate, Template
+import os
+import distutils.dir_util
 
 
 def copy_dir_to_scaffold(output_dir, package, dir):
@@ -17,19 +13,6 @@ def copy_dir_to_scaffold(output_dir, package, dir):
 class AtramhasisTemplate(PyramidTemplate):
     _template_dir = 'atramhasis_scaffold'
     summary = 'Create an Atramhasis implementation'
-
-    def pre(self, command, output_dir, vars):
-        from alembic import command
-        from alembic.script import ScriptDirectory
-        root = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
-        alembic_config = Config(os.path.join(root, 'alembic.ini'))
-        alembic_config.set_main_option(
-            'script_location', os.path.join(root, 'atramhasis', 'alembic')
-        )
-        scripts = ScriptDirectory.from_config(alembic_config)
-        vars['last_revision'] = scripts.get_revisions("head")[0].revision
-        vars['today'] = datetime.datetime.now().isoformat()
-        super().pre(command, output_dir, vars)
 
     def post(self, command, output_dir, vars):  # pragma: no cover
         """ Overrides :meth:`pyramid.scaffolds.template.Template.post`"""
