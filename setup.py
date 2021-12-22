64f2554d77c91f3be76348b7ded4a3d3db4e498d
Bram Goessens <bram.goessens@vlaanderen.be>
2021-12-22 15:53:38 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ requires = [
 ]
 
 setup(name='atramhasis',
-      version='0.7.0',
+      version='1.0.0',
       description='A web based editor for thesauri adhering to the SKOS specification.',
       long_description=README + '\n\n' + CHANGES,
       long_description_content_type='text/x-rst',
