938b0cd518d92d3b5a44046f35289067f4dd0f50
Jonas Geuens <jonasgeuens@gmail.com>
2023-02-07 08:38:48 +0100

    #767 op.execute oneline instead of get bind

--- atramhasis/alembic/versions/0978347c16f9_rename_flemish_vlaams.py
+++ atramhasis/alembic/versions/0978347c16f9_rename_flemish_vlaams.py
@@ -16,9 +16,7 @@ revision = '0978347c16f9'
 down_revision = '88214d86a9d1'
 
 def upgrade():
-    connection = op.get_bind()
-    connection.execute("UPDATE language SET name = 'Vlaams' WHERE id = 'vls'")
+    op.execute("UPDATE language SET name = 'Vlaams' WHERE id = 'vls'")
     
 def downgrade():
-    connection = op.get_bind()
-    connection.execute("UPDATE language SET name = 'Flemish' WHERE id = 'vls'")
+    op.execute("UPDATE language SET name = 'Flemish' WHERE id = 'vls'")
