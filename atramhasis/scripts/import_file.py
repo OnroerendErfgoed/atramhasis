3d25c818cfe0449d6d12949425cd586c4448466b
Tim Van Campenhout <45393594+vancamti@users.noreply.github.com>
2023-06-07 16:29:47 +0200

    #774 update import_file script to add provider (#837)
    * #774 import_file script
    
    * #774 review fix
    
    * #774 review fix
    
    * #774 more review fixes
    
    * #774 more review fixes
    
    * #774 more review fixes
    
    * #774 more review fixes
    
    * #774 more review fixes

--- atramhasis/scripts/import_file.py
+++ atramhasis/scripts/import_file.py
@@ -7,18 +7,18 @@ import sys
 from rdflib import Graph
 from rdflib.util import SUFFIX_FORMAT_MAP
 from rdflib.util import guess_format
-from skosprovider.skos import ConceptScheme
 from skosprovider.providers import DictionaryProvider
 from skosprovider.providers import SimpleCsvProvider
+from skosprovider.skos import ConceptScheme
 from skosprovider.uri import UriPatternGenerator
 from skosprovider_rdf.providers import RDFProvider
-from skosprovider_sqlalchemy.models import Label
-from skosprovider_sqlalchemy.models import Note
-from skosprovider_sqlalchemy.models import Source
 from skosprovider_sqlalchemy.utils import import_provider
 from sqlalchemy import create_engine
 from sqlalchemy.engine import url
 from sqlalchemy.orm import sessionmaker
+
+from atramhasis.data.models import Provider
+from atramhasis.scripts.migrate_sqlalchemy_providers import json_serial
 
 
 def file_to_rdf_provider(**kwargs) -> RDFProvider:
@@ -27,10 +27,11 @@ def file_to_rdf_provider(**kwargs) -> RDFProvider:
     """
     input_file = kwargs.get('input_file')
     input_name, input_ext = os.path.splitext(os.path.basename(input_file))
+    meta_id = kwargs.get("provider_id") or input_name.upper()
     graph = Graph()
     graph.parse(input_file, format=guess_format(input_ext))
     return RDFProvider(
-        {'id': input_name.upper()},
+        {'id': meta_id},
         graph
     )
 
@@ -52,11 +53,12 @@ def file_to_csv_provider(**kwargs) -> SimpleCsvProvider:
     """
     input_file = kwargs.get('input_file')
     input_name, input_ext = os.path.splitext(os.path.basename(input_file))
+    meta_id = kwargs.get("provider_id") or input_name.upper()
     provider_kwargs = _create_provider_kwargs(**kwargs)
     with open(input_file) as ifile:
         reader = csv.reader(ifile)
         return SimpleCsvProvider(
-            {'id': input_name.upper()},
+            {'id': meta_id},
             reader,
             **provider_kwargs
         )
@@ -68,11 +70,12 @@ def file_to_json_provider(**kwargs) -> DictionaryProvider:
     """
     input_file = kwargs.get('input_file')
     input_name, input_ext = os.path.splitext(os.path.basename(input_file))
+    meta_id = kwargs.get("provider_id") or input_name.upper()
     provider_kwargs = _create_provider_kwargs(**kwargs)
     with open(input_file) as data_file:
         dictionary = json.load(data_file)
     return DictionaryProvider(
-        {'id': input_name.upper()},
+        {'id': meta_id},
         dictionary,
         **provider_kwargs
     )
@@ -106,48 +109,77 @@ def parse_argv_for_import(argv):
         description='Import file to a database',
         epilog=(
             f'example: {cmd} '
-            '--from atramhasis/scripts/my_file '
+            'atramhasis/scripts/my_file '
+            'urn:x-skosprovider:trees:%s '
             '--to sqlite:///atramhasis.sqlite '
-            '--conceptscheme_label Labels '
-            '--conceptscheme_uri urn:x-skosprovider:trees '
-            '--uri_pattern urn:x-skosprovider:trees:%s'
+            '--conceptscheme-label Labels '
+            '--conceptscheme-uri urn:x-skosprovider:trees '
+            '--create-provider '
+            '--provider-id ERFGOEDTYPES '
+            '--id-generation-strategy numeric'
         )
     )
-    parser.add_argument('--from',
-                        dest='input_file',
-                        type=str,
-                        help='local path to the input file',
-                        required=True
-                        )
-    parser.add_argument('--to',
-                        dest='to',
-                        metavar='conn_string',
-                        type=str,
-                        help='Connection string of the output database',
-                        required=False,
-                        default='sqlite:///atramhasis.sqlite'
-                        )
-    parser.add_argument('--conceptscheme_label',
-                        dest='cs_label',
-                        type=str,
-                        help='Label of the conceptscheme',
-                        required=False,
-                        default=None
-                        )
-    parser.add_argument('--conceptscheme_uri',
-                        dest='cs_uri',
-                        type=str,
-                        help='URI of the conceptscheme',
-                        required=False,
-                        default=None
-                        )
-    parser.add_argument('--uri_pattern',
-                        dest='uri_pattern',
-                        type=str,
-                        help='URI pattern input for the URIGenerator',
-                        required=False,
-                        default=None
-                        )
+    parser.add_argument(
+        'input_file',
+        type=str,
+        help='local path to the input file',
+    )
+    parser.add_argument(
+        'uri_pattern',
+        type=str,
+        help='URI pattern input for the URIGenerator',
+    )
+    parser.add_argument(
+        '--to',
+        dest='to',
+        metavar='conn_string',
+        type=str,
+        help='Connection string of the output database',
+        required=False,
+        default='sqlite:///atramhasis.sqlite'
+    )
+    parser.add_argument(
+        '--conceptscheme-label',
+        dest='cs_label',
+        type=str,
+        help='Label of the conceptscheme',
+        required=False,
+        default=None
+    )
+    parser.add_argument(
+        '--conceptscheme-uri',
+        dest='cs_uri',
+        type=str,
+        help='URI of the conceptscheme',
+        required=False,
+        default=None
+    )
+    parser.add_argument(
+        '--create-provider',
+        dest='create_provider',
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help='An optional parameter if given a provider is created. '
+             'Use --no-create-provider to not create a provider',
+    )
+    parser.add_argument(
+        '--provider-id',
+        dest='provider_id',
+        type=str,
+        help='An optional string (eg. ERFGOEDTYPES) to be assigned to the provider id. '
+             'If not specified, assign the conceptscheme id to the provider id',
+        required=False,
+        default=None
+    )
+    parser.add_argument(
+        '--id-generation-strategy',
+        dest='id_generation_strategy',
+        type=str,
+        help='URI pattern input for the URIGenerator',
+        required=False,
+        choices=["numeric", "guid", "manual"],
+        default="numeric"
+    )
     args = parser.parse_args()
     if not validate_file(args.input_file) or not validate_connection_string(args.to):
         sys.exit(1)
@@ -209,7 +241,15 @@ def create_conceptscheme(conceptscheme_uri: str, conceptscheme_label: str) -> Co
 def main(argv=sys.argv):
     """
     Documentation: import -h
-    Run: import --from <path_input_file> --to <conn_string> --conceptscheme_label <cs_label> --conceptscheme_uri <cs_uri> --uri_pattern <uri_pattern>
+    Run: import
+    <path_input_file>
+    <uri_pattern>
+    --to <conn_string>
+    --conceptscheme-uri <cs_uri>
+    --conceptscheme-label <cs_label>
+    --create-provider
+    --provider-id <provider_id>
+    --id-generation-strategy <numeric/guid/manual>
 
     example path_input_file:
      atramhasis/scripts/my_file
@@ -232,33 +272,65 @@ def main(argv=sys.argv):
     args = parse_argv_for_import(argv)
     input_name, input_ext = os.path.splitext(os.path.basename(args.input_file))
     session = conn_str_to_session(args.to)
-    file_to_provider_function = [supported_types[filetype]['file_to_provider'] for filetype in supported_types.keys()
-                                 if input_ext in supported_types[filetype]['extensions']][0]
+    file_to_provider_function = [
+        supported_types[filetype]['file_to_provider']
+        for filetype in supported_types.keys()
+        if input_ext in supported_types[filetype]['extensions']
+    ][0]
     if args.cs_uri:
         cs_uri = args.cs_uri
         cs_label = args.cs_label if args.cs_label else input_name.capitalize()
         args.concept_scheme = create_conceptscheme(cs_uri, cs_label)
     provider = file_to_provider_function(**vars(args))
     cs = import_provider(provider, session)
+    if args.create_provider:
+        db_provider = Provider()
+        provider.metadata[
+            'atramhasis.id_generation_strategy'
+        ] = args.id_generation_strategy.upper()
+        db_provider.meta = json.loads(json.dumps(provider.metadata, default=json_serial))
+        db_provider.expand_strategy = 'RECURSE'
+        db_provider.conceptscheme = cs
+        db_provider.id = args.provider_id or cs.id
+        db_provider.uri_pattern = args.uri_pattern
+        if 'conceptscheme_id' in db_provider.meta:
+            del db_provider.meta['conceptscheme_id']
+        session.add(db_provider)
+    session.commit()
 
     # Get info to return to the user
-    prov_id = input_name.upper()
     scheme_id = cs.id
-    print("\n\n*** The import of conceptscheme {0} from the {1} file to {2} was succesful. ***\
-          \n\nTo use the data in Atramhasis, you must edit the file my_thesaurus/skos/__init__.py.\
-          \nAdd a configuration similar to:\
-            \n\ndef create_registry(request):\
-            \n\t# create the SKOS registry\
-            \n\tregistry = Registry(instance_scope='threaded_thread')\
-            \n\t{3} = SQLAlchemyProvider(\
-            \n\t\t{{'id': '{4}', 'conceptscheme_id': {5}}},\
-            \n\t\trequest.db\
-            \n\t)\
-            \n\tregistry.register_provider({6})\
-            \n\treturn registry\
-            \n\n".
-          format(prov_id, args.input_file, args.to,
-                 prov_id.replace(' ', '_'), prov_id, scheme_id, prov_id.replace(' ', '_')))
+    if not args.create_provider:
+        prov_id = getattr(args, 'provider_id', None) or input_name.upper()
+        print(
+            "\n\n*** The import of conceptscheme {0} from the {1} file to {2} was succesful. ***\
+              \n\nTo use the data in Atramhasis, you must edit the file my_thesaurus/skos/__init__.py.\
+              \nAdd a configuration similar to:\
+                \n\ndef create_registry(request):\
+                \n\t# create the SKOS registry\
+                \n\tregistry = Registry(instance_scope='threaded_thread')\
+                \n\t{3} = SQLAlchemyProvider(\
+                \n\t\t{{'id': '{4}', 'conceptscheme_id': {5}}},\
+                \n\t\trequest.db\
+                \n\t)\
+                \n\tregistry.register_provider({6})\
+                \n\treturn registry\
+                \n\n".
+            format(
+                prov_id, args.input_file, args.to,
+                prov_id.replace(' ', '_'), prov_id, scheme_id, prov_id.replace(' ', '_')
+            )
+        )
+    else:
+        prov_id = args.provider_id or cs.id
+        msg = """
+***
+The import of conceptscheme {0} from the {1} file to {2} was succesful.
+You can now continue through the Atramhasis UI.
+***
+"""
+        print(msg.format(prov_id, args.input_file, args.to))
+
 
 
 if __name__ == '__main__':
--- tests/test_import_scripts.py
+++ tests/test_import_scripts.py
@@ -7,8 +7,10 @@ from skosprovider.skos import Concept
 from skosprovider.skos import Note
 from skosprovider.utils import dict_dumper
 from skosprovider_sqlalchemy.providers import SQLAlchemyProvider
+from sqlalchemy import select
 
 import tests
+from atramhasis.data.models import Provider
 from atramhasis.scripts import import_file
 from tests import DbTest
 from tests import SETTINGS
@@ -84,6 +86,11 @@ class ImportTests(DbTest):
         self.assertIsInstance(eb.notes[0], Note)
         self.assertIn('Mornay', eb.notes[0].note)
         self.assertEqual('note', eb.notes[0].type)
+        provider = self.session.execute(select(Provider)).scalar_one()
+        self.assertEqual(provider.meta['atramhasis.id_generation_strategy'], 'NUMERIC')
+        self.assertEqual(provider.uri_pattern, uri_pattern)
+        self.assertEqual(provider.expand_strategy.value, 'recurse')
+        self.assertEqual(provider.id, str(provider.conceptscheme_id))
 
     def _check_parrots(self, conceptscheme_label):
         sql_prov = SQLAlchemyProvider({'id': 'PARROTS', 'conceptscheme_id': 1}, self.session)
@@ -101,41 +108,87 @@ class ImportTests(DbTest):
         assert blauwereiger in reiger.narrower
 
     def test_import_rdf(self):
-        sys.argv = ['import_file', '--from', test_data_rdf, '--to', SETTINGS['sqlalchemy.url']]
+        sys.argv = [
+            'import_file',
+            test_data_rdf,
+            'http://id.trees.org/%s',
+            '--to', SETTINGS['sqlalchemy.url']
+        ]
         import_file.main(sys.argv)
         tests.db_filled = True
         self._check_trees('Verschillende soorten bomen')
 
     def test_import_ttl(self):
-        sys.argv = ['import_file', '--from', test_data_ttl, '--to', SETTINGS['sqlalchemy.url']]
+        sys.argv = [
+            'import_file',
+            test_data_ttl,
+            'http://id.trees.org/%s',
+            '--to', SETTINGS['sqlalchemy.url'],
+        ]
         import_file.main(sys.argv)
         tests.db_filled = True
         self._check_trees('Different types of trees')
 
     def test_import_ttl_string_id(self):
-        sys.argv = ['import_file', '--from', test_data_ttl_string_id, '--to', SETTINGS['sqlalchemy.url']]
+        sys.argv = [
+            'import_file',
+            test_data_ttl_string_id,
+            'http://id.trees.org/%s',
+            '--to', SETTINGS['sqlalchemy.url']
+        ]
         import_file.main(sys.argv)
         tests.db_filled = True
 
     def test_import_json(self):
-        sys.argv = ['import_file', '--from', test_data_json,
-                    '--to', SETTINGS['sqlalchemy.url'],
-                    '--conceptscheme_label', 'Trees Conceptscheme', '--conceptscheme_uri', 'http://id.trees.org',
-                    '--uri_pattern', 'http://id.trees.org/%s']
+        sys.argv = [
+            'import_file',
+            test_data_json,
+            'http://id.trees.org/%s',
+            '--to', SETTINGS['sqlalchemy.url'],
+            '--conceptscheme-label', 'Trees Conceptscheme',
+            '--conceptscheme-uri', 'http://id.trees.org',
+        ]
         import_file.main(sys.argv)
         tests.db_filled = True
         self._check_trees('Trees Conceptscheme')
 
     def test_import_csv(self):
-        sys.argv = ['import_file', '--from', test_data_csv, '--to', SETTINGS['sqlalchemy.url']]
+        sys.argv = [
+            'import_file',
+            test_data_csv,
+            'urn:x-skosprovider:menu:%s',
+            '--to', SETTINGS['sqlalchemy.url'
+            ],
+        ]
         import_file.main(sys.argv)
         tests.db_filled = True
         self._check_menu()
 
     def test_import_csv_uri_generator(self):
-        sys.argv = ['import_file', '--from', test_data_csv, '--to', SETTINGS['sqlalchemy.url'],
-                    '--conceptscheme_label', 'Menu Conceptscheme', '--conceptscheme_uri', 'http://id.menu.org',
-                    '--uri_pattern', 'http://id.menu.org/%s']
+        sys.argv = [
+            'import_file', test_data_csv, 'http://id.menu.org/%s',
+            '--to', SETTINGS['sqlalchemy.url'],
+            '--conceptscheme-label', 'Menu Conceptscheme',
+            '--conceptscheme-uri', 'http://id.menu.org'
+        ]
         import_file.main(sys.argv)
         tests.db_filled = True
         self._check_menu('http://id.menu.org/%s')
+
+    def test_import_csv_with_provider_all_args(self):
+        sys.argv = [
+            'import_file',
+            test_data_csv,
+            'urn:x-skosprovider:test:%s',
+            '--to', SETTINGS['sqlalchemy.url'],
+            '--provider-id', 'MENU',
+            '--create-provider',
+            '--id-generation-strategy', 'guid',
+        ]
+        import_file.main(sys.argv)
+        tests.db_filled = True
+        provider = self.session.execute(select(Provider)).scalar_one()
+        self.assertEqual(provider.meta['atramhasis.id_generation_strategy'], 'GUID')
+        self.assertEqual(provider.uri_pattern, 'urn:x-skosprovider:test:%s')
+        self.assertEqual(provider.expand_strategy.value, 'recurse')
+        self.assertEqual(provider.id, 'MENU')
