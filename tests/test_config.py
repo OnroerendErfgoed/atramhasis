4cbd2550732d699d78475f38da54bd5e046cb6b5
Wim De Clercq <4599667+Wim-De-Clercq@users.noreply.github.com>
2025-11-21 09:47:15 +0100

    Refactor test using tempfile.
    Issue #946

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,12 @@
 import os
-import random
-import shutil
+import tempfile
 
 from pyramid.paster import get_appsettings
 
 from atramhasis import main
 from tests import DbTest
 from tests import setup_db
+
 
 here = os.path.dirname(__file__)
 settings = get_appsettings(os.path.join(here, '../', 'tests/conf_test.ini'))
@@ -17,15 +17,12 @@ def setUpModule():
 
 
 class TestConfig(DbTest):
-
     def test_config(self):
         app = main({}, **settings)
         self.assertIsNotNone(app)
 
     def test_config_alt_dump_location(self):
-        random_dump_loc = f'dump{random.randint(0, 100)}'
-        settings['atramhasis.dump_location'] = os.path.join(here, random_dump_loc)
-        app = main({}, **settings)
-        self.assertIsNotNone(app)
-        if os.path.exists(random_dump_loc):
-            shutil.rmtree(random_dump_loc)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            settings['atramhasis.dump_location'] = temp_dir
+            app = main({}, **settings)
+            self.assertIsNotNone(app)
