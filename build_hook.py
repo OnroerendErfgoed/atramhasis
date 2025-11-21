7fd2562ae74f70681af833b18e1ad80f1b068c4b
Wim De Clercq <4599667+Wim-De-Clercq@users.noreply.github.com>
2025-11-21 08:59:24 +0100

    Remove compiled message catalog files.
    Compilation is automated when building a wheel.
    Developers should compile them themselves.
    
    Issue #900

--- build_hook.py
+++ build_hook.py
@@ -6,12 +6,31 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class BuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data) -> None:
-        """Build frontend when building the wheel."""
+        """Execute compilations when building the wheel."""
         super().initialize(version, build_data)
 
+        self.build_frontend()
+        self.compile_message_catalog()
+
+    def compile_message_catalog(self):
         root_dir = Path(__file__).parent
-        static = root_dir / "atramhasis" / "static"
-        static_admin = static / "admin"
-        subprocess.run(["npm", "install"], cwd=static, check=True)
-        subprocess.run(["npm", "install"], cwd=static_admin, check=True)
-        subprocess.run(["grunt", "-v", "build"], cwd=static_admin, check=True)
+        subprocess.run(
+            [
+                'pybabel',
+                'compile',
+                '--directory',
+                'atramhasis/locale',
+                '--domain',
+                'atramhasis',
+            ],
+            cwd=root_dir,
+            check=True,
+        )
+
+    def build_frontend(self):
+        root_dir = Path(__file__).parent
+        static = root_dir / 'atramhasis' / 'static'
+        static_admin = static / 'admin'
+        subprocess.run(['npm', 'install'], cwd=static, check=True)
+        subprocess.run(['npm', 'install'], cwd=static_admin, check=True)
+        subprocess.run(['grunt', '-v', 'build'], cwd=static_admin, check=True)
