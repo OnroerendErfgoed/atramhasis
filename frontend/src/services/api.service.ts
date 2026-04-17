ddd5dcf005e8f605fda39ba620266853c0275ea9
Axel Verstappen <axel.verstappen@gmail.com>
2026-04-17 10:55:12 +0200

    #1016 added overview on ID

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,4 +1,5 @@
 import type { ConceptScheme } from '@models/conceptscheme';
+import type { Concept } from '@models/concept';
 import { HttpService } from './http.service';
 
 export class ApiService extends HttpService {
@@ -8,5 +9,9 @@ export class ApiService extends HttpService {
 
   async getConceptschemes(): Promise<ConceptScheme[]> {
     return (await this.get<ConceptScheme[]>('/conceptschemes')).data;
+  }
+
+  async getConceptscheme(schemeId: string, options: { type?: string; sort?: string }): Promise<Concept[]> {
+    return (await this.get<Concept[]>(`/conceptschemes/${schemeId}/c`, { params: options })).data;
   }
 }
