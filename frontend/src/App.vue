1c6ec442540507c000e71e807a83b62d5ef9b7fd
wouter-adriaens <69460082+wouter-adriaens@users.noreply.github.com>
2026-04-23 10:56:56 +0200

    Epic 1029/feature/1022 add provider (#1046)
    * #1022 wip
    
    * #1022 Add provider
    
    * #1022 Refactor language to list store
    
    * #1022 Move modalprovider locale to components section
    
    * #1022 Use top-level actions locale
    
    * #1022 Introduce generic validation and api messages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,3 +3,14 @@
     <RouterView />
   </UApp>
 </template>
+
+<script setup lang="ts">
+import { useListStore } from '@stores/list';
+import { onBeforeMount } from 'vue';
+
+const listStore = useListStore();
+
+onBeforeMount(async () => {
+  await listStore.getAll();
+});
+</script>
