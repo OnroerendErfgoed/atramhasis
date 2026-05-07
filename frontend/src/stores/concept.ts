b7abcf9465444d8e6d7220c79bf7cb9e95c8fd7b
wouter-adriaens <wouter@wauwit.be>
2026-05-07 15:46:33 +0200

    #1019 Omit empty fields when checking merge uniqueness

--- frontend/src/stores/concept.ts
+++ frontend/src/stores/concept.ts
@@ -1,7 +1,8 @@
 import type { Concept } from '@models/concept';
+import type { Label, Note, Source } from '@models/util';
 import { ApiService } from '@services/api.service';
 import { useMatchStore } from '@stores/match';
-import { isEqual, uniqWith } from 'lodash-es';
+import { isEmpty, isEqual, omitBy, uniqWith } from 'lodash-es';
 import { defineStore, storeToRefs } from 'pinia';
 import { ref, watch } from 'vue';
 import { useAdminUiStore } from './admin-ui';
@@ -33,6 +34,9 @@ export const useConceptStore = defineStore('concept', () => {
 
   const resetSelectedConcept = () => (selectedConcept.value = undefined);
 
+  const isEqualIgnoringEmptyFields = (left: Label | Note | Source, right: Label | Note | Source): boolean =>
+    isEqual(omitBy(left, isEmpty), omitBy(right, isEmpty));
+
   const mergeWithSelectedConcept = (matchUris: string[]): Concept | undefined => {
     if (!selectedConcept.value) {
       return;
@@ -46,15 +50,16 @@ export const useConceptStore = defineStore('concept', () => {
     // Merge labels, notes and sources from linked concepts into selected concept, ensuring uniqueness
     selectedConcept.value.labels = uniqWith(
       [...selectedConcept.value.labels, ...linkedConcepts.flatMap((c) => c.labels)],
-      isEqual
+      isEqualIgnoringEmptyFields
     );
+
     selectedConcept.value.notes = uniqWith(
       [...selectedConcept.value.notes, ...linkedConcepts.flatMap((c) => c.notes)],
-      isEqual
+      isEqualIgnoringEmptyFields
     );
     selectedConcept.value.sources = uniqWith(
       [...selectedConcept.value.sources, ...linkedConcepts.flatMap((c) => c.sources)],
-      isEqual
+      isEqualIgnoringEmptyFields
     );
 
     return selectedConcept.value;
