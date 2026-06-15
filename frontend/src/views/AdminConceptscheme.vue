1899003b79df1c5f3384b7c8cdee52fbe0fad7f4
Axel Verstappen <axel.verstappen@gmail.com>
2026-06-15 10:59:43 +0200

    Readded font-medium to title

--- frontend/src/views/AdminConceptscheme.vue
+++ frontend/src/views/AdminConceptscheme.vue
@@ -42,7 +42,7 @@
     >
       <template #label-cell="{ row }">
         <div>
-          {{ row.original.label }}
+          <span class="font-medium">{{ row.original.label }}</span>
           <div class="mt-0.5 flex items-center gap-1 text-xs text-muted">
             <span>{{ row.original.uri }}</span>
             <ClipboardCopy
